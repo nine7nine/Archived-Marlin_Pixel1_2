d449526a93e8f7c9fa8f57e44dcae4277550d373
Humberto Borba <humberos@gmail.com>
2017-07-18 21:23:41 -0300

    drivers: hwmon: Fix direct references to HZ
    Make the various timeout values HZ agnostic by using the proper
    macros and values instead.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I5963a7959954a45c1d36789f88c2736e5717b1b0
    (cherry picked from commit afe120d07248e2b46bb1bacb1c03f9cf035a1df6)
    (cherry picked from commit d92573270e35c4585dd177e59e37e563d41744a2)
    (cherry picked from commit d017627daafee85f5874aaf7fcd7ce874d35ab0b)
    (cherry picked from commit 5c8f8d56ad52da53765f8e649c5651e5bff4f85f)
    (cherry picked from commit 1e8a37757d3338ffa20e8643a6b4fcbf8ccccaf1)
    (cherry picked from commit 2bd08a927a76743b059efcc56f4293384ada21f2)
    (cherry picked from commit e8f46c3b0bdd1f90b765d8d26f2916ec841e07f4)
    (cherry picked from commit 7f9f63be7a58f1ba2228ff4d42b9acff1b0524f9)
    (cherry picked from commit 0d3f11fb7d54053fce76fdf47de57c2aadb4b04c)
    (cherry picked from commit 706f215248689a30d58919dc85e2fe48253f3248)
    (cherry picked from commit c1cd1e36c2b0c829d019281e0505270b773a4aee)
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/hwmon/qpnp-adc-voltage.c
+++ drivers/hwmon/qpnp-adc-voltage.c
@@ -104,7 +104,7 @@
 #define QPNP_VADC_CONV_TIMEOUT_ERR				2
 #define QPNP_VADC_CONV_TIME_MIN					1000
 #define QPNP_VADC_CONV_TIME_MAX					1100
-#define QPNP_ADC_COMPLETION_TIMEOUT				HZ
+#define QPNP_ADC_COMPLETION_TIMEOUT				1000
 #define QPNP_VADC_ERR_COUNT					20
 #define QPNP_OP_MODE_SHIFT					3
 
@@ -370,6 +370,7 @@ static int32_t qpnp_vadc_status_debug(struct qpnp_vadc_chip *vadc)
 
 	return 0;
 }
+
 static int32_t qpnp_vadc_configure(struct qpnp_vadc_chip *vadc,
 			struct qpnp_adc_amux_properties *chan_prop)
 {
@@ -1668,7 +1669,7 @@ recalibrate:
 	} else {
 		rc = wait_for_completion_timeout(
 					&vadc->adc->adc_rslt_completion,
-					QPNP_ADC_COMPLETION_TIMEOUT);
+					msecs_to_jiffies(QPNP_ADC_COMPLETION_TIMEOUT));
 		if (!rc) {
 			rc = qpnp_vadc_read_reg(vadc, QPNP_VADC_STATUS1,
 							&status1, 1);
