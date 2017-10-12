4630a3143a286a899ed3ba382680b73fbe9322d7
freak07 <michalechner92@googlemail.com>
2017-10-12 09:39:25 +0200

    Revert "DEBUG: sched: add tracpoint for energy/performance variations"
    This reverts commit fd2c0c4eef40093ecc14569c345d47e9c0f99bb1.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5009,7 +5009,6 @@ static inline int __energy_diff(struct energy_env *eenv)
 	__update_perf_energy_deltas(eenv);
 
 	trace_sched_energy_diff(eenv);
-	trace_sched_energy_perf_deltas(eenv);
 
 	return eenv->nrg_delta;
 }
