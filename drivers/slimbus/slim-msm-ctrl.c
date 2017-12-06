c14c8078da4dbad362738b1b5533b60c7dc7fc6d
ninez <johnstonljordan@gmail.com>
2017-12-05 23:44:44 -0500

    fix slimbus / merge

--- drivers/slimbus/slim-msm-ctrl.c
+++ drivers/slimbus/slim-msm-ctrl.c
@@ -92,7 +92,7 @@ enum mgr_intr {
 
 enum frm_cfg {
 	FRM_ACTIVE	= 1,
-	CLK_GEAR	= 7,
+	CLK_GEAR	= 10,
 	ROOT_FREQ	= 11,
 	REF_CLK_GEAR	= 15,
 	INTR_WAKE	= 19,
@@ -207,7 +207,7 @@ static irqreturn_t msm_slim_interrupt(int irq, void *d)
 		 * signalling completion/exiting ISR
 		 */
 		mb();
-		msm_slim_manage_tx_msgq(dev, false, NULL);
+		msm_slim_manage_tx_msgq(dev, false, NULL, 0);
 	}
 	if (stat & MGR_INT_RX_MSG_RCVD) {
 		u32 rx_buf[10];
