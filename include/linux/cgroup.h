6eefe46b4512af103ce174abca036c2381d148df
ninez <johnstonljordan@gmail.com>
2018-02-10 03:01:37 -0500

    cgroups fixups
    Signed-off-by: ninez <johnstonljordan@gmail.com>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -955,6 +955,8 @@ int subsys_cgroup_allow_attach(struct cgroup_subsys_state *css,
 
 #else /* !CONFIG_CGROUPS */
 
+struct cgroup_subsys_state;
+
 static inline int cgroup_init_early(void) { return 0; }
 static inline int cgroup_init(void) { return 0; }
 static inline void cgroup_fork(struct task_struct *p) {}
@@ -966,6 +968,8 @@ static inline int cgroupstats_build(struct cgroupstats *stats,
 {
 	return -EINVAL;
 }
+
+static inline void css_put(struct cgroup_subsys_state *css) {}
 
 /* No cgroups - nothing to do */
 static inline int cgroup_attach_task_all(struct task_struct *from,
