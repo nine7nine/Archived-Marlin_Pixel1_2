00c0b045a7aa0fa76d456bdd1aaf51caa48b748a
RenderBroken <zkennedy87@gmail.com>
2018-05-02 10:05:30 -0400

    Revert "ANDROID: sched/rt: add schedtune accounting"
    This reverts commit 44063920ade7de45fc5bce4ee9796aa7e2af3aa5.

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -10,7 +10,6 @@
 #include <linux/irq_work.h>
 
 #include "walt.h"
-#include "tune.h"
 
 int sched_rr_timeslice = RR_TIMESLICE;
 
@@ -1313,7 +1312,6 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	if (!task_current(rq, p) && p->nr_cpus_allowed > 1)
 		enqueue_pushable_task(rq, p);
 
-	schedtune_enqueue_task(p, cpu_of(rq));
 	sched_rt_update_capacity_req(rq);
 }
 
@@ -1327,7 +1325,6 @@ static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 
 	dequeue_pushable_task(rq, p);
 
-	schedtune_dequeue_task(p, cpu_of(rq));
 	sched_rt_update_capacity_req(rq);
 }
 
