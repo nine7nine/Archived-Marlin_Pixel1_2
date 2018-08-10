faf399f6b3799281a0038d3a7b21645aaa8f3cad
jordan Johnston <johnstonljordan@gmail.com>
2018-08-10 01:56:27 -0400

    LTS: 3.18.116

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -212,6 +212,10 @@ static const struct usb_device_id usb_quirk_list[] = {
 	{ USB_DEVICE(0x1c75, 0x0204), .driver_info =
 			USB_QUIRK_CONFIG_INTF_STRINGS },
 
+	/* Corsair Strafe */
+	{ USB_DEVICE(0x1b1c, 0x1b15), .driver_info = USB_QUIRK_DELAY_INIT |
+	  USB_QUIRK_DELAY_CTRL_MSG },
+
 	/* Corsair K70 LUX */
 	{ USB_DEVICE(0x1b1c, 0x1b36), .driver_info = USB_QUIRK_DELAY_INIT },
 
